e8083bc97cc874c793b15561edfedd5bbd8b866b
mortuus <mortuusars@gmail.com>
2023-01-31 11:03:33 +0200

    Slightly fixed Salted tooltip.

--- src/main/java/io/github/mortuusars/salt/event/ClientEvents.java
+++ src/main/java/io/github/mortuusars/salt/event/ClientEvents.java
@@ -2,11 +2,14 @@ package io.github.mortuusars.salt.event;
 
 import io.github.mortuusars.salt.Salt;
 import io.github.mortuusars.salt.Salting;
+import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.network.chat.TranslatableComponent;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ForgeModelBakery;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
+
+import java.util.List;
 
 public class ClientEvents {
 
@@ -16,8 +19,8 @@ public class ClientEvents {
 
     public static void onItemTooltipEvent(ItemTooltipEvent event) {
         if (Salting.isSalted(event.getItemStack())) {
-            event.getToolTip()
-                    .add(new TranslatableComponent("salt.gui.tooltip.salted").withStyle(Style.EMPTY.withColor(0xF0D8D5)));
+            List<Component> toolTip = event.getToolTip();
+            toolTip.add(toolTip.size() >= 1 ? 1 : 0, new TranslatableComponent("salt.gui.tooltip.salted").withStyle(Style.EMPTY.withColor(0xF0D8D5)));
         }
     }
 }
