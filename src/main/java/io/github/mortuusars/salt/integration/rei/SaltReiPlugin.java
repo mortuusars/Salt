aff5c4d52e11da2a157cdcd45ecaa6630b1425f1
mortuus <mortuusars@gmail.com>
2023-02-27 03:53:18 +0200

    Logo.

---
+++ src/main/java/io/github/mortuusars/salt/integration/rei/SaltReiPlugin.java
@@ -0,0 +1,10 @@
+package io.github.mortuusars.salt.integration.rei;
+
+
+//import me.shedaniel.rei.api.common.plugins.REIServerPlugin;
+
+import me.shedaniel.rei.api.common.plugins.REIServerPlugin;
+
+//@REIPluginCommon
+//public class SaltReiPlugin implements REIServerPlugin {
+//}
