55bda5fd687af49c803a3fe21e5e2a04014c22c4
mortuusars <mortuusars@gmail.com>
2023-12-23 08:41:08 +0200

    Added Salt item to forge:salt, forge:dusts and forge:dusts/salt tags.

--- src/main/java/io/github/mortuusars/salt/data/provider/ItemTags.java
+++ src/main/java/io/github/mortuusars/salt/data/provider/ItemTags.java
@@ -1,24 +1,33 @@
 package io.github.mortuusars.salt.data.provider;
 
 import io.github.mortuusars.salt.Salt;
+import net.minecraft.core.HolderLookup;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.data.tags.BlockTagsProvider;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
+import net.minecraftforge.common.data.BlockTagsProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.concurrent.CompletableFuture;
+
 public class ItemTags extends ItemTagsProvider {
-    public ItemTags(DataGenerator generator, BlockTagsProvider blockTagsProvider, @Nullable ExistingFileHelper existingFileHelper) {
-        super(generator, blockTagsProvider, Salt.ID, existingFileHelper);
+    public ItemTags(DataGenerator generator, CompletableFuture<HolderLookup.Provider> pLookupProvider, BlockTagsProvider blockTagsProvider, @Nullable ExistingFileHelper existingFileHelper) {
+        super(generator.getPackOutput(), pLookupProvider, blockTagsProvider.contentsGetter(), Salt.ID, existingFileHelper);
     }
 
+
     @Override
-    protected void addTags() {
+    protected void addTags(HolderLookup.@NotNull Provider provider) {
         tag(Salt.ItemTags.FORGE_TORCHES)
                 .add(Items.TORCH);
+
+        tag(TagKey.create(ForgeRegistries.ITEMS.getRegistryKey(), new ResourceLocation("forge:salt")));
+        tag(TagKey.create(ForgeRegistries.ITEMS.getRegistryKey(), new ResourceLocation("forge:dusts")));
+        tag(TagKey.create(ForgeRegistries.ITEMS.getRegistryKey(), new ResourceLocation("forge:dusts/salt")));
 
         tag(Salt.ItemTags.FORGE_SALTS)
                 .add(Salt.Items.SALT.get());
